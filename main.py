611fc1deba49f571bc5ed8133d377c0f314e1fa4
Jay Mistry <117970099+jaymistry1511@users.noreply.github.com>
2024-09-06 11:40:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Mail id generator:
+x = input("Enter your first name: ")
+y = input("Enter your surname: ")
+z = input("Enter your birth year: ")
+
+print("Your mail id is: " + x + y + z + "." + x[0] + y[0] + "@gmail.com")
